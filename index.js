3ae2815b8758a76623895dd1f5d3561f193a39b7
OisinWrites <oisin.banville@gmail.com>
2022-07-14 13:59:09 +0000

    add css style to form

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ input.addEventListener("change", function() {
     const reader = new FileReader();
     reader.addEventListener("load", () => {
       const uploaded_image = reader.result;
-      document.querySelector("#displayimage").style.backgroundImage = url(${uploaded_image});
+      document.querySelector("#displayimage").style.backgroundImage = `url(${uploaded_image})`;
     });
     reader.readAsDataURL(this.files[0]);
   });
