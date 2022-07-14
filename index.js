3a9b69ec1c3a2fad9a6918d53000f40b87c85ebc
OisinWrites <oisin.banville@gmail.com>
2022-07-14 13:53:04 +0000

    add javascript for form

---
+++ index.js
@@ -0,0 +1,10 @@
+const input=document.querySelector("#userimages");
+
+input.addEventListener("change", function() {
+    const reader = new FileReader();
+    reader.addEventListener("load", () => {
+      const uploaded_image = reader.result;
+      document.querySelector("#displayimage").style.backgroundImage = url(${uploaded_image});
+    });
+    reader.readAsDataURL(this.files[0]);
+  });
